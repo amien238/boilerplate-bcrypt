b129f1d258804913aebf7b59615f3a57018317be
amien238 <amien23804@gmail.com>
2025-08-19 19:57:38 +0700

    test

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 'use strict';
-const bcrypt = require('bcrypt');
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
@@ -10,6 +9,7 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+const bcrypt = require('bcrypt');
 
 
 
